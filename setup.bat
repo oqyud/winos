5a8226ecaa3a137fdec2cf6a9787be76cbc0b2bf
YuYu <go.bin043120@gmail.com>
2024-09-29 21:24:34 +0300

    mmm

--- setup.bat
+++ setup.bat
@@ -2,7 +2,7 @@
 
 cd /d "%~dp0\modules"
 
-call .\init.bat
-call .\packages.bat
-call .\configurations.bat
-call .\mounts.bat
+call init.bat
+call packages.bat
+call configurations.bat
+call mounts.bat
