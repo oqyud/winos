4f13144d77197cf62db71d9c46df65b92f8cccfa
oqyud <go.bin043120@gmail.com>
2024-11-16 15:27:31 +0300

    123

--- data/mounts/shared.bat
+++ data/mounts/shared.bat
@@ -23,3 +23,15 @@ set "to_1=%USERPROFILE%\Videos\Davinci Resolve"
 rd /s /q "%to_1%"
 mklink /D "%to_1%" "%from_1%"
 endlocal
+
+:: Bash
+setlocal
+set "from_1=%storage-settings%\User Folder\.bashrc"
+set "to_1=%USERPROFILE%\.bashrc"
+set "from_2=%storage-settings%\User Folder\.inputrc"
+set "to_2=%USERPROFILE%\.inputrc"
+rd /s /q "%to_1%"
+rd /s /q "%to_2%"
+mklink "%to_1%" "%from_1%"
+mklink "%to_2%" "%from_2%"
+endlocal
